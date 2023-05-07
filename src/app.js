d70dbbd21a5350c5af8dd0e7970e539bdfc7cbaa
Ryan Dougan <dougieled@hotmail.co.uk>
2023-05-07 21:09:28 +0800

    change name

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ const ApiError = require('./utils/ApiError');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Live!');
+  res.send('Live Event-Backend!');
 });
 
 if (config.env !== 'test') {
